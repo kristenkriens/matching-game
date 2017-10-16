1cb358207e17747edf884dcdc92405b333524cc0
Kristen Kriens <kristen.kriens@gmail.com>
2017-10-16 11:33:56 -0400

    Add js code to manage clicks, generate random animals, and show back of tile on click

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,30 @@
-// $(function() {
-//   var tileWidth = $('.game__board-tile').width();
-//   console.log(tileWidth)
-//   $('.game__board-tile').css({'height': tileWidth + 'px'});
-// });
+$(function() {
+  var games = {
+    clicks: 0,
+    score: 0
+  }
+
+  var animals = ['bear', 'beaver', 'cat', 'cow', 'deer', 'dog', 'eagle', 'elephant', 'fox', 'frog', 'giraffe', 'hedgehog', 'hippo', 'koala', 'lion', 'llama', 'monkey', 'mouse', 'owl', 'panda', 'parrot', 'penguin', 'pig', 'raccoon', 'seal', 'sheep', 'sloth', 'squirrel', 'tiger', 'wolf'];
+
+  // Generate random number from 1 to some number
+  var randomNum = function(num) {
+    return Math.floor(Math.random() * num);
+  }
+
+  // Generate random animal based on randomNum() function
+  function randomAnimal(animals) {
+    var i = randomNum(animals.length);
+    var animal = animals[i];
+    return animal;
+  }
+
+  $('.game__board-tile').on('click', function() {
+    if(!$(this).hasClass('game__board-tile--flipped')) {
+      games.clicks++;
+    }
+
+    $('.game__stats-clicks span').text(games.clicks);
+
+    $(this).addClass('game__board-tile--flipped');
+  });
+});
