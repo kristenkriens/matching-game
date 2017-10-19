ce8dd32a18b0c3ea3cbfe931eeaaa89b33a1b38f
Kristen Kriens <kristen.kriens@gmail.com>
2017-10-19 02:38:23 -0400

    Update time allotted to 1 minute

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,7 @@ $(function() {
   var games = {
     clicks: 0,
     score: 0,
-    time: '1:30'
+    time: '1:00'
   }
 
   var animals = ['bear', 'beaver', 'cat', 'cow', 'deer', 'dog', 'eagle', 'elephant', 'fox', 'frog', 'giraffe', 'hedgehog', 'hippo', 'koala', 'lion', 'llama', 'monkey', 'mouse', 'owl', 'panda', 'parrot', 'penguin', 'pig', 'raccoon', 'seal', 'sheep', 'sloth', 'squirrel', 'tiger', 'wolf', 'anteater', 'baboon', 'bison', 'boar', 'capybara', 'crocodile', 'dove', 'duck', 'fennec-fox', 'goat', 'guinea-pig', 'horse', 'kangaroo', 'lemur', 'mole', 'moose', 'ostrich', 'platypus', 'rabbit', 'rooster', 'skunk', 'snake', 'sparrow', 'swan', 'turtle', 'chameleon', 'puffin'];
